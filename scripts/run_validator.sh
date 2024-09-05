6601fa963693f4f4efa7259246bb6ee4ff081dc9
kamil <kamil.wojciechowski@chain-insights.ai>
2024-09-06 00:06:22 +0200

    adjustment for run validator

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -3,9 +3,13 @@
 python3 -m venv venv_validator
 source venv_validator/bin/activate
 pip install -r requirements.txt
+
+cp -r env venv_validator/
+
 export PYTHONPATH=$(pwd)
 echo "PYTHONPATH is set to $PYTHONPATH"
 NETWORK_TYPE=${1:-mainnet}
 cd src
 python3 subnet/cli.py $NETWORK_TYPE
 
+deactivate
