8890eaca5808113203dfa391b53d7109ef590c46
kamil <kamil.wojciechowski@chain-insights.ai>
2024-09-11 21:47:04 +0200

    scoring fix

--- src/subnet/validator/validator.py
+++ src/subnet/validator/validator.py
@@ -223,7 +223,7 @@ class Validator(Module):
         #    numeric_tolerance=0.05, string_threshold=80)
 
         similarity_score = 0
-        score = +0.3 * similarity_score
+        score += 0.3 * similarity_score
 
         multiplier = min(1, receipt_miner_multiplier)
         score += 0.4 * multiplier
