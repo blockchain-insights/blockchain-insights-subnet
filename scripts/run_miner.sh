0c5267e9538c14126be61e171bb6c5cc1a8f6c7f
kamil <kamil.wojciechowski@chain-insights.ai>
2024-09-16 11:43:18 +0200

    updated startup scripts

--- scripts/run_miner.sh
+++ scripts/run_miner.sh
@@ -3,8 +3,13 @@
 python3 -m venv venv_miner
 source venv/bin/activate
 pip install -r requirements.txt
+
+cp -r env venv_miner/
+
 export PYTHONPATH=$(pwd)
 echo "PYTHONPATH is set to $PYTHONPATH"
 NETWORK_TYPE=${1:-mainnet}
 cd src
 python3 subnet/miner/miner.py $NETWORK_TYPE
+
+deactivate
--- scripts/run_validator_api.sh
+++ scripts/run_validator_api.sh
@@ -3,9 +3,13 @@
 python3 -m venv venv_validator_api
 source venv_validator/bin/activate
 pip install -r requirements.txt
+
+cp -r env venv_validator_api/
+
 export PYTHONPATH=$(pwd)
 echo "PYTHONPATH is set to $PYTHONPATH"
 NETWORK_TYPE=${1:-mainnet}
 cd src
 python3 subnet/validator/validator_api.py $NETWORK_TYPE
 
+deactivate
