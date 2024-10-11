3c1edecf56a251242ec74bb74f8004fa366da2fe
Dmytro Savenkov <dmytro.savenkov@chain-insights.ai>
2024-10-11 17:55:24 +0300

    Miner hardcode fixed

--- src/subnet/validator/validator.py
+++ src/subnet/validator/validator.py
@@ -354,8 +354,6 @@ class Validator(Module):
         timestamp = datetime.utcnow()
         query_hash = generate_hash(query)
 
-        miner_key="5Hmxvb5J2PsUwDE3MUQGNqopxaGMDCG8V1EZ24JM6hMMTcJH"
-
         if miner_key:
             miner = await self.miner_discovery_manager.get_miner_by_key(miner_key, network)
             if not miner:
